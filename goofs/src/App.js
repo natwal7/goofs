cbb0607fd9d6ea5eb2cb5aeb5edc1c04f83de341
exampleCoderOne <exampleemail3001@gmail.com>
2023-04-28 22:47:52 -0700

    first commit

---
+++ goofs/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p className="title">No more CSS rules!!!!!</p>
+        {/* <img src="https://media.istockphoto.com/id/1328395253/vector/hundred-points-emoji-icon.jpg?s=612x612&w=0&k=20&c=zy2NI8jM4FQhOuIqv4EfkqRvLamb-DeL4DDhqBKsS74=" /> */}
+        <img className="shrek" src="https://o.remove.bg/downloads/bd3ce0fa-c2b4-415d-abe6-d0043fd0538c/download-removebg-preview.png"></img>
+        <img className="mike" src="https://o.remove.bg/downloads/95039909-2199-4eeb-836b-4bd8978c8048/images-removebg-preview.png"></img>
+        <img className="hundred" src="https://o.remove.bg/downloads/3a27ef1a-a779-46d5-819a-180b1325fd0b/istockphoto-1408259940-612x612-removebg-preview.png"></img>
+        <img className="amongUs" src="https://o.remove.bg/downloads/4514d3cd-150c-4e78-b577-8a3a56db3e08/download-removebg-preview.png"></img>
+        <img className="faceEmojiOne" src="https://o.remove.bg/downloads/72efbf8f-f01a-4bf0-8029-77c6bffc6c2e/291740432035211-removebg-preview.png"></img>
+        <img className="faceEmojiTwo" src="https://o.remove.bg/downloads/72efbf8f-f01a-4bf0-8029-77c6bffc6c2e/291740432035211-removebg-preview.png"></img>
+        <p className="arrow"> --> </p>
+        
+       
+
+
+
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
